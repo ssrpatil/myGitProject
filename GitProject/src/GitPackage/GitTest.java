a29b562720223b83b2cfed8bbd08ebd7e76b89e2
ssrpatil <42009760+ssrpatil@users.noreply.github.com>
2018-08-02 01:03:42 +0530

    Update GitTest.java

--- GitProject/src/GitPackage/GitTest.java
+++ GitProject/src/GitPackage/GitTest.java
@@ -4,7 +4,7 @@ public class GitTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Welcome TO GitHub");
+System.out.println("Git To Eclipse");
 	}
 
 }
